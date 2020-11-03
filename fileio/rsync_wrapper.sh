fc97f79dea3ec2fd01f90b84630fe2c920873dd6
dbrannonhoward <dbrannonhoward@gmail.com>
2020-11-03 14:04:21 -0500

    file paths changed

--- fileio/rsync_wrapper.sh
+++ fileio/rsync_wrapper.sh
@@ -1,10 +1,10 @@
 # init
 user_home="$(getent passwd $USER | awk -F ':' '{print $6}')"
-source_from_home="/start/"
+source_from_home="/BashTools/SANDBOX/start/"
 source_path=$user_home$source_from_home
 source_filename="file.txt"
 source_filepath=$source_path$source_filename
-destination_from_home="/finish/"
+destination_from_home="/BashTools/SANDBOX/finish/"
 destination_path=$user_home$destination_from_home
 args="-uvah --progress --stats"
 # command
