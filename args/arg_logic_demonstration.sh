98e180a50d226867dd59fcba29437c510ac943b5
dbrannonhoward <dbrannonhoward@gmail.com>
2020-11-04 20:54:51 -0500

    added exact arg detection

--- args/arg_logic_demonstration.sh
+++ args/arg_logic_demonstration.sh
@@ -5,6 +5,11 @@ then
 elif [ -z "$1" ]
 then
 	echo "empty string used as arg"
+elif [ $input_arg == "some_match" ]
+then
+	echo -e "exact argument detected :"
+	echo -e "\t$input_arg"
 else
-	echo $input_arg
+	echo -e "unhandled argument detected :"
+	echo -e "\t$input_arg"
 fi
