d1a175919a80959b15510c3b148f38497124c07f
dbrannonhoward <dbrannonhoward@gmail.com>
2020-11-04 21:43:52 -0500

    add names arg to script

---
+++ appinfo/print_installed_applications.sh
@@ -0,0 +1,14 @@
+temp=installed.txt
+if [ $# -eq 0 ]
+then
+	apt list --installed
+elif [ $1 == "names" ]
+then
+	#TODO improve this by not writing to disk
+	apt list --installed > $temp
+	awk -F'/' '{ print $1 }' ./$temp
+	rm ./$temp
+else
+	echo -e "argument not recognized, try : "
+	echo -e "\tnames"
+fi
