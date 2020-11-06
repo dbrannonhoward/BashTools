794f82b1e2530b9486e324854f8301c0fd792196
dbrannonhoward <dbrannonhoward@gmail.com>
2020-11-05 20:53:54 -0500

    function examples

--- demo_function/function_demonstration.sh
+++ demo_function/function_demonstration.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
-echo "there are a two ways to declare a function"
+echo -e "there are a two ways to declare a function\n"
 function one_way_to_declare_a_function {
 	echo "this is one way to declare a function"
 }
+
 another_way_to_declare_a_function () {
 	echo "this is another way to declare a function"
 }
+one_way_to_declare_a_function && another_way_to_declare_a_function
+
 arg_processor () {
-	echo "arg : $1"
+	echo "your arg was : $1"
 }
+
 arg_returner () {
 	var=$1
 	return $var
 }
-echo -e "the functions must be declared on a line 'above' where they are called"
-one_way_to_declare_a_function
-another_way_to_declare_a_function
-arg_processor potato
-arg_returner 2
-echo "the returned value was $?"
+
+arg_reader () {
+	echo "the most recently returned value was $?"
+}
+arg_processor potato && arg_returner 2 && arg_reader
+
+echo -e "\nnote : the functions must be declared on a line 'above' where they are called"
