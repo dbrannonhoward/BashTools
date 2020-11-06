d859f8244afd7bc17d007d1af6d52219d0981826
dbrannonhoward <dbrannonhoward@gmail.com>
2020-11-06 01:02:32 -0500

    shelved for now, relative path issues

--- launchers/python_print_working_dir.sh
+++ launchers/python_print_working_dir.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#TODO, relative directory issues
-python_script_path="../python/"
+#TODO this may be broken due to relative path stuff, fix me
+parent_path=$( cd "$(dirname "${BASH_SOURCE}[0]}")" ; pwd -P )
+python_script_path=$parent_path$( "python" )
 python_module_name="print_working_dir_to_terminal.py"
 command python $python_script_path$python_module_name
--- launchers/script_caller.sh
+++ launchers/script_caller.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+#TODO this is broken due to relative path stuff, fixme later
 all_args="\tbash\n\tsh\n\tsource"
-path_to_bad_args_func="../common/bad_args.sh"
+self_script_directory=$( dirname -- "$(readlink -f -- "$BASH_SOURCE")")
+path_to_common=$self_script_dir"../common/"
+path_to_bad_args=$path_to_common"bad_args.sh"
 script_to_call="./called_script.sh"
 bad_args () {
 	$path_to_bad_args_func $all_args
 }
-if [ $# -eq 0 ];
+if [ $# -eq 0 ]
 then
-	$script_to_call
-elif [ $# -eq 2 ];
+	output=$script_to_call
+	echo $output
+elif [ $# -eq 2 ]
 then
 	caller="$1"
 	script="$2"
@@ -18,16 +22,21 @@ then
 	else
 		bad_args
 	fi
-elif [ $1 == "bash" ];
+elif [ $1 == "bash" ]
 then
 	bash $script_to_call
-elif [ $1 == "sh" ];
+elif [ $1 == "eval" ]
+then
+	eval $script_to_call
+elif [ $1 == "exec" ]
+then
+	exec $script_to_call
+elif [ $1 == "sh" ]
 then
 	sh $script_to_call
-elif [ $1 == "source" ];
+elif [ $1 == "source" ]
 then
 	source $script_to_call
-
 else
 	bad_args
 fi
