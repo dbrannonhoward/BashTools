b356782120e91f07c1dc29977e3e11ca9a2baf18
dbrannonhoward <dbrannonhoward@gmail.com>
2020-11-05 23:06:06 -0500

    added a python caller

---
+++ launchers/python_print_working_dir.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#TODO, relative directory issues
+python_script_path="../python/"
+python_module_name="print_working_dir_to_terminal.py"
+command python $python_script_path$python_module_name
---
+++ python/print_working_dir_to_terminal.py
@@ -0,0 +1,10 @@
+import os
+
+def print_working_dir():
+    print(os.getcwd())
+
+def main():
+    print_working_dir()
+
+if __name__ == '__main__':
+    main()
