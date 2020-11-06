49881ef784d5b510c2e6bbfbcaa0bbcacf4386b2
dbrannonhoward <dbrannonhoward@gmail.com>
2020-11-06 10:19:31 -0500

    reduced

--- demo_fileio/rsync_wrapper_demo.sh
+++ demo_fileio/rsync_wrapper_demo.sh
@@ -1,13 +1,7 @@
-# init
-user_home="$(getent passwd $USER | awk -F ':' '{print $6}')"
-source_from_home="/BashTools/SANDBOX/start/"
-source_path=$user_home$source_from_home
-source_filename="file.txt"
-source_filepath=$source_path$source_filename
-destination_from_home="/BashTools/SANDBOX/finish/"
-destination_path=$user_home$destination_from_home
+#!/bin/bash
+file="file"
+source_filepath="$(getent passwd $USER | awk -F ':' '{print $6}')""/BashTools/SANDBOX/start/"$file
+destination_path="$(getent passwd $USER | awk -F ':' '{print $6}')""/BashTools/SANDBOX/finish/"
 args="-uvah --progress --stats"
-# command
 rsync $args $source_filepath $destination_path
-# cleanup
-rm $destination_path$source_filename
+rm $destination_path$file
