777631b3216b4c51d54a909094f9f536b7b9a26f
dbrannonhoward <dbrannonhoward@gmail.com>
2020-11-09 18:42:52 -0500

    add filter

--- python/p2_builtin_map.py
+++ python/p2_builtin_map.py
@@ -3,5 +3,4 @@ def plus_one(num):
     return num + 1
 
 a = [1, 3, 5, 7]
-b = map(plus_one, a)
-print b
+print map(plus_one, a)
--- python/p3_builtin_map.py
+++ python/p3_builtin_map.py
@@ -2,5 +2,4 @@
 def plus_one(num):
     return num + 1
 a = [1, 3, 5, 7]
-b = map(plus_one, a)
-print(list(b))
+print(list(map(plus_one, a)))
