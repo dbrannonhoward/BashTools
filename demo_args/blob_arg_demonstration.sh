298b3dc0e298cd2e187565ee8f94e7ffe488c891
dbrannonhoward <dbrannonhoward@gmail.com>
2020-11-13 00:12:45 -0500

    fix bug, add blob args

--- demo_args/blob_arg_demonstration.sh
+++ demo_args/blob_arg_demonstration.sh
@@ -5,11 +5,13 @@ then
 elif [ -z "$1" ]
 then
 	echo "empty string used as arg"
-elif [ $1 == "match" ]
+elif [ "$1" == match ]
 then
 	echo "exact argument detected :"
 	echo -e "\t$1"
 else
-	echo "unhandled argument detected :"
-	echo -e "\t$1"
+	for ARG in $@; do
+		echo "unhandled argument detected :"
+		echo -e "\t$ARG"
+	done
 fi
