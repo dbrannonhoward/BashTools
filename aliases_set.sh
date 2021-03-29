d19ed95cf1d6d34b06a884f8718710b165f9b73b
nonbhowardST <55259160+nonbhoward@users.noreply.github.com>
2021-03-29 19:22:54 -0400

    bug fix file path

--- aliases_set.sh
+++ aliases_set.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-home="~/"
+home="$(getent passwd $USER | awk -F ':' '{print $6}')/"
 bash=$home".bash_aliases"
-if [ -f $bash ]
-then
+if [ -f $bash ];then
 	echo $bash" already exists"
 else
 	echo "copying project alias to "$bash
 	#cp ./bash_aliases ~/.bash_aliases
 fi
+
