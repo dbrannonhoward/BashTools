2192d5ebc463f2364f562e748a2e2cdb2f920896
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-06-08 02:29:38 -0400

    fix: better vars & conventions

--- aliases_get.sh
+++ aliases_get.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+# set local paths
 path_home="$(getent passwd $USER | awk -F ':' '{print $6}')/"
-fn_bash=$path_home".bash_aliases"
+fn_alias_local=".bash_aliases"
+path_alias_local=$path_home$fn_alias_local
+echo "path to bash alias on client = $path_alias_local"
 
-rpo_aliases="./bash_aliases"
+# set repo paths
+path_repo="$PWD/"
+fn_alias_repo="bash_aliases"
+path_alias_repo=$path_repo$fn_alias_repo
+echo "path to bash alias in repo = $path_alias_repo"
 
-if [ -f $fn_bash ];then
-	echo $fn_bash" exists on local, concat to repo"
-	cat $fn_bash >> $rpo_aliases
+# perform get action
+if [ -f $path_bash ];then
+	echo "$fn_alias_local exists on local, concat to repo"
+	# cat $fn_alias_local >> $fn_alias_repo
 else
-	echo "copying project alias to "$fn_bash
+	echo "copying project alias to $fn_alias_local"
 fi
 
