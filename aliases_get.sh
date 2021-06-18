a1875afd7afff70bf7b0f080d9680380e505605c
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-06-18 17:48:32 -0400

    add: dquotes to stop str split

--- aliases_get.sh
+++ aliases_get.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # set local paths
-path_home="$(getent passwd $USER | awk -F ':' '{print $6}')/"
+path_home="$(getent passwd "$USER" | awk -F ':' '{print $6}')/"
 fn_alias_local=".bash_aliases"
 path_alias_local=$path_home$fn_alias_local
 echo "path to bash alias on client = $path_alias_local"
@@ -13,10 +13,9 @@ path_alias_repo=$path_repo$fn_alias_repo
 echo "path to bash alias in repo = $path_alias_repo"
 
 # perform get action
-if [ -f $path_bash ];then
+if [ -f "$path_alias_local" ];then
 	echo "$fn_alias_local exists on local, concat to repo"
 	# cat $fn_alias_local >> $fn_alias_repo
 else
 	echo "copying project alias to $fn_alias_local"
 fi
-
