d037a1733ffea8a25ad177bf5477d1e691d05308
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-07-18 14:09:31 -0400

    add: app installer

---
+++ install_applications_from_list.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# script function : install applications to a ubuntu-based 
+#		    distributin from a formatted text file
+# requirements : a formatted text file
+# text file format : one application per line
+# text file example :
+# 	htop
+#	python3
+#	vim
+
+# system specific variables
+USER="username"
+APPFILE="/home/$USER/system/applications.txt"
+
+clear
+echo "SCRIPT STARTED"
+echo -e "\tAPPFILE=$APPFILE"
+
+echo "ENSURING $APPFILE EXISTS"
+if [ -f "$APPFILE" ];then
+  echo -e "\t$APPFILE exists"
+else
+  echo -e "\t$APPFILE does not exist" && echo "exit program" && exit
+fi
+
+echo "READING $APPFILE"
+APPLINES=$(cat $APPFILE)
+
+echo "BUILDING COMMAND"
+COMMAND="sudo apt-get install"
+for APPLINE in "$APPLINES"
+do
+  COMMAND+=" $APPLINE"
+done
+echo -e "executing $COMMAND" | tr '\n' ' '
+$COMMAND
+
